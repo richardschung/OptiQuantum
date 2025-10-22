c1eff2b77538441f565831216c6f59da34f146d1
richardschung <richard.chung@colostate.edu>
2025-10-21 19:28:27 -0600

    Update Clements_4x4_Haar_fidelity_test_no_int.py

--- OptiQuantum/figure_generation/Clements_4x4_Haar_fidelity_test_no_int.py
+++ OptiQuantum/figure_generation/Clements_4x4_Haar_fidelity_test_no_int.py
@@ -8,7 +8,7 @@ import sys
 from matplotlib import rc
 from matplotlib.ticker import FormatStrFormatter
 from pytictoc import TicToc
-from strawberryfields import rectangular_symmetric
+from strawberryfields.decompositions import rectangular_symmetric
 
 sys.path.append('../../')
 from neuroptica.layers_new import ClementsLayer
