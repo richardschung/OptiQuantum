70c77d6c9e4ee1b956fb6db44f230dec3a35232a
Richard Chung <richard.chung@colostate.edu>
2025-10-27 11:01:40 -0600

    Additional Graphs

--- OptiQuantum_tests/figure_generation/visibility_plotter.py
+++ OptiQuantum_tests/figure_generation/visibility_plotter.py
@@ -12,7 +12,7 @@ def main():
     n_phase = int(max_phase/step_size_phase) + 1
     phases = np.linspace(0,max_phase,n_phase)
 
-    fidels = np.loadtxt(f'figures_set1/fidelity_vs_mean_phases_Clements_8x8_1000samples_1Haar_unc_pi_32.txt')
+    visibs = np.loadtxt(f'figures_set1/visibiity_vs_random_phases_Clements_1000samples_MZI_1.txt')
 
     cmap='gist_heat'
 
@@ -33,7 +33,7 @@ def main():
     # Plot Loss + Phase uncert accuracies
     # plt.pcolor(self.loss_dB, self.phase_uncert_theta, self.accuracy_LPU, vmin=100/(self.N+1)*0, vmax=100, cmap=cmap, rasterized=True)
     #plt.figure(figsize=(6.95, 5.03)) # compress the graph (around) quarter in size, by cutting top half and compress horizontally
-    plt.pcolor(phases, phases, fidels.T, cmap=cmap, rasterized=True, vmin=0, vmax=1)
+    plt.pcolor(phases, phases, visibs.T, cmap=cmap, rasterized=True, vmin=0, vmax=1)
 
     ax = plt.gca()
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
@@ -41,17 +41,17 @@ def main():
     ax.tick_params(axis='both', which='minor', labelsize=tick_size)
     ax.tick_params(axis='both', which='major', labelsize=tick_size)
 
-    plt.xlabel(r'Mean Error in $\theta$ (rad)', fontsize=labels_size)
-    plt.ylabel(r'Mean Error in $\phi$(rad)', fontsize=labels_size)
+    plt.xlabel(r'Standard Deviation of $\theta$ (rad)', fontsize=labels_size)
+    plt.ylabel(r'Standard Deviation of $\phi$(rad)', fontsize=labels_size)
     cbar = plt.colorbar()
     cbar.ax.tick_params(labelsize=tick_size)
-    cbar.set_label('Fidelity', fontsize=labels_size)
+    cbar.set_label('Visibility', fontsize=labels_size)
     # plt.title(f'{self.N}$\\times${self.N} {self.topology}', fontsize=labels_size)
     plt.tight_layout()
 
     #plt.show()
 
-    plt.savefig(f'figures_set1/Update October 26/fidelity_vs_mean_phases_Clements_8x8_1000samples_1Haar_unc_pi_32.png')
+    plt.savefig(f'figures_set1/Update October 26/visibility_vs_phase_sd_Clements_1000samples_MZI_1.png')
 
 if __name__ == '__main__':
     main()
